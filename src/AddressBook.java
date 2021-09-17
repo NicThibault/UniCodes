5920f9b5d052dd2ea17c3a330b1b64dd17aebe54
Nicholas Thibault <nicholas_thibault@hotmail.com>
2021-09-17 11:54:28 -0400

    Adding "Address Book" print to main of AddressBook.java.

---
+++ src/AddressBook.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo> buddyList = new ArrayList<>();
+
+    public AddressBook() {}
+
+    public AddressBook(ArrayList<BuddyInfo> buddyList) {
+        this.buddyList = buddyList;
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        this.buddyList.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        this.buddyList.remove(buddy);
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Address Book");
+        /*
+        BuddyInfo buddy1 = new BuddyInfo("Homer", "742 Evergreen Terrace", "555-8707");
+
+        BuddyInfo buddy2 = new BuddyInfo("bruh", "1738", "123-4567");
+
+        AddressBook address = new AddressBook();
+
+        address.addBuddy(buddy1);
+
+        System.out.println(buddy1.toString());
+
+        address.addBuddy(buddy2);
+
+        System.out.println(buddy2.toString());
+
+        address.removeBuddy(buddy1);
+
+        String list = address.buddyList.toString();
+
+        System.out.println(list);
+        */
+    }
+
+
+}
