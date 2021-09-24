e12cce838467c964625b62b4caa34bec17571864
NicThibault <91337794+nicthibault@users.noreply.github.com>
2021-09-24 13:19:47 -0400

    Delete BuddyInfo

---
+++ src/AddressBook.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo> buddyList = new ArrayList<>();
+
+    public AddressBook() {}
+
+    public AddressBook(ArrayList<BuddyInfo> buddyList) {
+        this.buddyList = buddyList;
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        this.buddyList.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        this.buddyList.remove(buddy);
+    }
+
+
+    public static void main(String[] args) {
+
+        //Change to test github
+
+        BuddyInfo buddy1 = new BuddyInfo("Homer", "742 Evergreen Terrace", "555-8707");
+
+        AddressBook address = new AddressBook();
+
+        address.addBuddy(buddy1);
+
+        address.removeBuddy(buddy1);
+
+
+    }
+
+
+}
